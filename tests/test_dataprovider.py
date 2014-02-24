6186ed21a0ccad83c911bcca12f7a22efd898d59
i.pavlov <i.pavlov@2gis.ru>
2014-02-24 15:27:56 +0700

    fix provider function name encoding from unicode arguments

--- tests/test_dataprovider.py
+++ tests/test_dataprovider.py
@@ -15,7 +15,7 @@ tests_to_run = [
     "test_function_dataprovider_1",
     "test_function_dataprovider_2",
     "test_function_dataprovider_3",
-    u"test_unicode_string_dataprovider_первый",
+    "test_unicode_string_dataprovider_первый",
     u"test_unicode_string_dataprovider_второй",
     "test_class_dataprovider_1",
     "test_class_dataprovider_2",
@@ -55,7 +55,7 @@ class DataproviderTest(unittest.TestCase):
     def test_function_dataprovider(self, data):
         tests_to_run.remove("%s_%s" % ("test_function_dataprovider", data))
 
-    @dataprovider([u'первый', u'второй'])
+    @dataprovider(['первый', u'второй'])
     def test_unicode_string_dataprovider(self, data):
         tests_to_run.remove("%s_%s" % ("test_unicode_string_dataprovider", data))
 
