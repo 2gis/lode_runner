35e40de3a2cc90271fda9315f2c2ddbeb3f4c79e
i.pavlov <i.pavlov@2gis.ru>
2014-02-25 14:51:01 +0700

    add property provider

--- lode_runner/dataprovider.py
+++ lode_runner/dataprovider.py
@@ -23,22 +23,16 @@ class Dataprovider(Plugin):
         if isinstance(obj, list):
             tests = map(parent, obj)
         elif inspect.ismethod(obj):
-            test = parent(obj.__name__)
-            testMethod = _get_test_method(test)
-            data = testMethod._data_provided
-            _data = _prepare_data(data, test)
-            return _make_dataprovided_tests(testMethod, parent, _data)
+            tests = [parent(obj.__name__)]
 
         _tests = []
         for test in tests:
-            testMethod = _get_test_method(test)
-            if hasattr(testMethod, '_data_provided'):
-                data = testMethod._data_provided
-                _data = _prepare_data(data, test)
-                dataprovided_tests = _make_dataprovided_tests(testMethod, parent, _data)
-                _tests += dataprovided_tests
-            else:
-                _tests += [test]
+            _tests += _make_dataprovided_tests(test, parent)
+
+        tests = _tests
+        _tests = []
+        for test in tests:
+            _tests += _make_property_provided_tests(test, parent)
 
         tests = _tests
         if len(tests) > 0:
@@ -52,27 +46,66 @@ def _data_set_safe_name(data_set):
         return unicode(data_set).encode(encoding='utf-8')
 
 
-def _make_dataprovided_tests(testMethod, parent, data):
-    dataprovided_tests = []
-    for data_set in data:
-        name = testMethod.__name__ + "_" + _data_set_safe_name(data_set)
-        new_test_func = _make_func(testMethod, name, data_set)
-        setattr(parent, new_test_func.__name__, new_test_func)
-        new_test = parent(new_test_func.__name__)
-        dataprovided_tests.append(new_test)
+def _make_dataprovided_tests(test, parent):
+    testMethod = _get_test_method(test)
+    if hasattr(testMethod, '_data_provided'):
+        data = testMethod._data_provided
+        del testMethod._data_provided
+        _data = _prepare_data(data, test)
 
-    return dataprovided_tests
+        dataprovided_tests = []
+        for data_set in _data:
+            name = testMethod.__name__ + "_" + _data_set_safe_name(data_set)
+            new_test_func = _make_func(testMethod, name, data_set)
+            setattr(parent, new_test_func.__name__, new_test_func)
+            new_test = parent(new_test_func.__name__)
+            dataprovided_tests.append(new_test)
+
+        return dataprovided_tests
+    else:
+        return [test]
+
+
+def _make_property_provided_tests(test, parent):
+    testMethod = _get_test_method(test)
+    if hasattr(testMethod, '_property_provided'):
+        property_name, data = testMethod._property_provided
+        del testMethod._property_provided
+        _data = _prepare_data(data, test)
+
+        property_provided_tests = []
+        for data_set in _data:
+            new_parent = type(parent.__name__ + "_" + _data_set_safe_name(data_set), (parent,), {})
+            setattr(new_parent, property_name, data_set)
+            name = testMethod.__name__ + "_" + _data_set_safe_name(data_set)
+            new_test_func = _make_func(testMethod, name)
+            setattr(new_parent, new_test_func.__name__, new_test_func)
+            new_test = new_parent(new_test_func.__name__)
+            property_provided_tests.append(new_test)
+
+        return property_provided_tests
+    else:
+        return [test]
 
 
 def _get_test_method(test):
     return getattr(test, test._testMethodName).__func__
 
 
-def _make_func(func, name, data_set):
-    if not isinstance(data_set, tuple):
+def _make_data(data):
+    new_data = data
+    return new_data
+
+
+def _make_func(func, name, data_set=None):
+    if not isinstance(data_set, tuple) and data_set:
         data_set = (data_set, )
 
-    standalone_func = lambda *args: func(*(args + data_set))
+    if data_set:
+        standalone_func = lambda *args: func(*(args + data_set))
+    else:
+        standalone_func = lambda *args: func(*args)
+    standalone_func.__dict__.update(func.__dict__)
     standalone_func.__name__ = name
     return standalone_func
 
@@ -100,5 +133,16 @@ def dataprovider(data):
                 method.__func__._data_provided = data
         else:
             func._data_provided = data
+        return func
+    return wrapper
+
+
+def property_provider(property_name, data):
+    def wrapper(func):
+        if inspect.isclass(func):
+            for method_name, method in inspect.getmembers(func, predicate=inspect.ismethod):
+                method.__func__._property_provided = (property_name, data)
+        else:
+            func._property_provided = (property_name, data)
         return func
     return wrapper
